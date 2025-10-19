6d3d59cd601ff95d666c72209073ae244b1a651a
mojitog <mh.mojallal@gmail.com>
2025-10-19 16:51:06 +0200

    completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import openpyxl
 
 
 def get_inputs():
@@ -12,8 +13,9 @@ def down_sampling(img, first_pixel, size):
     avg_rgb = [0, 0, 0]
     for i in range(size):
         for j in range(size):
-            if (first_pixel[0]+i < size_x) and (first_pixel[0]+j < size_y):
+            if (first_pixel[0]+i < size_x) and (first_pixel[1]+j < size_y):
                 pixel = (first_pixel[0]+i, first_pixel[1]+j)
+                #print(pixel)
                 pixel_rgb = img.getpixel(pixel)
                 count += 1
                 for k in range(len(accumulative_rgb)):
@@ -33,9 +35,30 @@ def image_grid(img, sample_size):
         img_grid.append(column)      
     return img_grid
 
-sample_size = 10
+
+def tile_fill(img, first_pixel, size, avg_rgb):
+    size_x, size_y = img.size
+    for i in range(size):
+        for j in range(size):
+            if (first_pixel[0]+i < size_x) and (first_pixel[1]+j < size_y):
+                pixel = (first_pixel[0]+i, first_pixel[1]+j)
+                img.putpixel((first_pixel[0]+i, first_pixel[1]+j),
+                             (avg_rgb[0], avg_rgb[1], avg_rgb[2]))
+
+
+sample_size = 5
 img = Image.open("Heath_Ledger_as_the_Joker.JPG.webp")
 
 
-#print(down_sampling(img, (0,0), 10))
-print(image_grid(img, 500))
+img_wb = openpyxl.Workbook()
+ws = img_wb.active
+ws.append(("X", "Y", "R", "G", "B"))
+img_grid = image_grid(img, sample_size)
+for col in img_grid:
+    for pixel in col:
+        avg_rgb = down_sampling(img, pixel, sample_size)
+        ws.append((pixel[0], pixel[1], avg_rgb[0], avg_rgb[1], avg_rgb[2]))
+        tile_fill(img, pixel, sample_size, avg_rgb)
+img_wb.save("img_grid.xlsx")
+
+img.show()
